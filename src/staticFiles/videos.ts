566db3c5c3e95ec137f159324d2ca66adfaf48c0
Vcky4 <okon.victor.u@gmail.com>
2023-05-26 11:15:27 +0100

    change video file to typescript

--- src/staticFiles/videos.ts
+++ src/staticFiles/videos.ts
@@ -1,6 +1,8 @@
-export default videos = [
+const videos = [
     { video: require("@assets/videos/dog.mp4") },
     { video: require("@assets/videos/girl.mp4") },
     { video: require("@assets/videos/ballon.mp4") },
     { video: require("@assets/videos/home-temp.mp4") },
   ]
+
+  export default videos
