1164e157b628cffadc41cfa9c778c8a0da18b5d9
Vcky4 <okon.victor.u@gmail.com>
2023-05-26 20:09:27 +0100

    create wallet and profile screen

---
+++ app/Profile.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+/**
+ * TODO - Rofile Screen
+ *
+ * @constructor
+ */
+
+export default function Profile(): ReactElement {
+    return(
+        <View>
+            <Text>Profile</Text>    
+        </View>
+    )
+}
---
+++ app/Wallet.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+
+/**
+ * TODO - Wallet Screen
+ *
+ * @constructor
+ */
+
+export default function Wallet(): ReactElement {
+    return(
+        <View>
+            <Text>Wallet</Text>    
+        </View>
+    )
+}
